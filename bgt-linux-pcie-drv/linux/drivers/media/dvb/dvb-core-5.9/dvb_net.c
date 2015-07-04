5d4ae0cdd45c105f70841e2aa7faf488214092c7
marcusbirkin <marcusbirkin@users.noreply.github.com>
2015-07-04 18:50:51 +0100

    Fix for change in alloc_netdev in kernel v3.17

--- bgt-linux-pcie-drv/linux/drivers/media/dvb/dvb-core-5.9/dvb_net.c
+++ bgt-linux-pcie-drv/linux/drivers/media/dvb/dvb-core-5.9/dvb_net.c
@@ -1277,9 +1277,15 @@ static int dvb_net_add_if(struct dvb_net *dvbnet, u16 pid, u8 feedtype)
 		return -EINVAL;
 	if ((if_num = get_if(dvbnet)) < 0)
 		return -EINVAL;
-
-	net = alloc_netdev(sizeof(struct dvb_net_priv), "dvb",
-			   dvb_net_setup);
+		
+        #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 17, 0)
+        net = alloc_netdev(sizeof(struct dvb_net_priv), "dvb",
+                           NET_NAME_UNKNOWN,
+                           dvb_net_setup);
+        #else
+        net = alloc_netdev(sizeof(struct dvb_net_priv), "dvb",
+                           dvb_net_setup);
+        #endif
 	if (!net)
 		return -ENOMEM;
 
