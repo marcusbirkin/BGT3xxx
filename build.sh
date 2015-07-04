013fc004fa4207b0600f1e7177b77917cfc4c1c0
marcusbirkin <marcusbirkin@users.noreply.github.com>
2015-07-04 18:54:18 +0100

    Force passing of kernel version to makefile

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ chmod u+x bgt-linux-pcie-drv/v4l/scripts/*.sh
 #Change v4l and make
 cd bgt-linux-pcie-drv/v4l
 make clean
-make -j4
+make -j4 KERNELRELEASE=$(uname -r) kernver=$(uname -r)
 #Check for clean exit
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
