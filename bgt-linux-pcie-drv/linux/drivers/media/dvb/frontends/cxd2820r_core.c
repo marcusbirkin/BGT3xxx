ed21ceb839e9fa408706cd054c3cb1744606ba01
marcusbirkin <marcusbirkin@users.noreply.github.com>
2017-04-08 14:17:28 +0100

    Fixes for changes in kernel v4.5
    http://lxr.free-electrons.com/source/include/linux/gpio/driver.h?v=4.5#L110

--- bgt-linux-pcie-drv/linux/drivers/media/dvb/frontends/cxd2820r_core.c
+++ bgt-linux-pcie-drv/linux/drivers/media/dvb/frontends/cxd2820r_core.c
@@ -736,7 +736,11 @@ struct dvb_frontend *cxd2820r_attach(const struct cxd2820r_config *cfg,
 #ifdef CONFIG_GPIOLIB
 		/* add GPIOs */
 		priv->gpio_chip.label = KBUILD_MODNAME;
-		priv->gpio_chip.dev = &priv->i2c->dev;
+		#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 5, 0)
+			priv->gpio_chip.parent = &priv->i2c->dev;
+		#else
+			priv->gpio_chip.dev = &priv->i2c->dev;
+		#endif
 		priv->gpio_chip.owner = THIS_MODULE;
 		priv->gpio_chip.direction_output =
 				cxd2820r_gpio_direction_output;
